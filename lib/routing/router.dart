0d5a3ea37e3874a2b179aa403996b97d2c856ab3
Mueed Qadri <mueedq12@gmail.com>
2023-01-12 13:09:04 -0500

    Initial commit

---
+++ lib/routing/router.dart
@@ -0,0 +1,30 @@
+import 'package:exampal/screens/home/chats/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:exampal/screens/home/home_screen.dart';
+import 'package:exampal/routing/routing_constants.dart';
+import 'package:exampal/screens/authenticate/sign_in.dart';
+import 'package:exampal/screens/authenticate/sign_up.dart';
+import 'package:exampal/routing/undefined_view.dart';
+import 'package:exampal/screens/wrapper.dart';
+
+import 'package:flutter/material.dart';
+
+import 'package:exampal/models/user.dart'; 
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+    
+     
+  switch (settings.name) {
+    case WrapperRoute:
+      return MaterialPageRoute(builder: (context) => Wrapper());
+    case SigninRoute:
+        return MaterialPageRoute(builder: (context) => SignInPage());
+    case SignupRoute:
+        return MaterialPageRoute(builder: (context) => SignUpScreen());
+    case ChatsRoute:
+      return MaterialPageRoute(builder: (context) => ChatScreen());
+    default:
+   
+    return MaterialPageRoute(builder: (context) => UndefinedView(name: settings.name,));
+  }
+}
---
+++ lib/routing/routing_constants.dart
@@ -0,0 +1,5 @@
+const String WrapperRoute = '/';
+const String SigninRoute = 'authenticate/signin';
+const String SignupRoute = 'authenticate/signup';
+const String HomeRoute = 'home/home';
+const String ChatsRoute = 'home/chats/chat_screen';
